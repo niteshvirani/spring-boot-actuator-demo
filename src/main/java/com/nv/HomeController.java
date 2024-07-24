5dfceb633c32d396986aa12849fbcd3f1c48eb9c
Nitesh Virani <niteshvirani@gmail.com>
2024-07-24 08:45:14 +0530

    first commit

---
+++ src/main/java/com/nv/HomeController.java
@@ -0,0 +1,17 @@
+package com.nv;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class HomeController {
+
+    private final HomeService homeService;
+
+    @GetMapping
+    public String home() {
+        return "hello, world!";
+    }
+}
